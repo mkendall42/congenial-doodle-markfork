ea1190aef6d7cfa40c008562eb11dba580fc3c67
Mark Kendall <mkendall.tutor@gmail.com>
2025-01-21 10:27:35 -0600

    Ride class: add board_rider() method and initial tests

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,5 +1,5 @@
 class Ride
-  attr_reader :name, :min_height, :admission_fee, :excitement, :total_revenue
+  attr_reader :name, :min_height, :admission_fee, :excitement, :total_revenue, :rider_log
 
   def initialize(ride_info_hash)
     @name = ride_info_hash[:name]
@@ -8,6 +8,18 @@ class Ride
     @excitement = ride_info_hash[:excitement]
 
     @total_revenue = 0
+    @rider_log = Hash.new(0)
   end
+
+  def board_rider(visitor)
+    #First, check if heigh is ok and thrill level?
+
+    rider_log[visitor] += 1
+  end
+
+  # def rider_log
+  #   #Generate a hash with visitor as key and # of times ridden as value
+  #   #Could just make in attr_reader really...
+  # end
 
 end
--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe Ride do
     @ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
     @visitor1 = Visitor.new('Bruce', 54, '$10')
     @visitor2 = Visitor.new('Tucker', 36, '$5')
+    @visitor1.add_preference(:gentle)
+    @visitor2.add_preference(:gentle)
   end
 
   it "exists" do
@@ -18,6 +20,15 @@ RSpec.describe Ride do
     expect(@ride1.admission_fee).to eq(1)
     expect(@ride1.excitement).to eq(:gentle)
     expect(@ride1.total_revenue).to eq(0)
+  end
+
+  it "tracks via log which visitors have ridden the ride" do
+    #Assume for now no other issues (e.g. height check)
+    @ride1.board_rider(@visitor1)
+    @ride1.board_rider(@visitor2)
+    @ride1.board_rider(@visitor1)
+
+    expect(@ride1.rider_log).to eq({@visitor1 => 2, @visitor2 => 1})
   end
 
 end
