6d478e854fd29bd4840f8a39e7185a3efbf62a6f
Mark Kendall <mkendall.tutor@gmail.com>
2025-01-21 10:50:24 -0600

    Ride class: complete board_rider() method and tests

--- lib/ride.rb
+++ lib/ride.rb
@@ -13,8 +13,12 @@ class Ride
 
   def board_rider(visitor)
     #First, check if heigh is ok and thrill level?
-
-    rider_log[visitor] += 1
+    if visitor.tall_enough?(@min_height) && visitor.preferences.include?(@excitement) && visitor.spending_money >= @admission_fee
+      rider_log[visitor] += 1
+      #Also make the visitor pay up!  (And add to revenue)
+      visitor.pay_fee(@admission_fee)   #Should always return true given our test above (or could factor that in above directly)
+      @total_revenue += @admission_fee
+    end
   end
 
   # def rider_log
--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -4,10 +4,15 @@ require './lib/ride.rb'
 RSpec.describe Ride do
   before(:each) do
     @ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+    @ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+    @ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
     @visitor1 = Visitor.new('Bruce', 54, '$10')
     @visitor2 = Visitor.new('Tucker', 36, '$5')
+    @visitor3 = Visitor.new('Penny', 64, '$15')
     @visitor1.add_preference(:gentle)
     @visitor2.add_preference(:gentle)
+    @visitor2.add_preference(:thrilling)
+    @visitor3.add_preference(:thrilling)
   end
 
   it "exists" do
@@ -22,13 +27,36 @@ RSpec.describe Ride do
     expect(@ride1.total_revenue).to eq(0)
   end
 
-  it "tracks via log which visitors have ridden the ride" do
-    #Assume for now no other issues (e.g. height check)
+  it "verifies visitor qualifies, and tracks via log which visitors have ridden" do
     @ride1.board_rider(@visitor1)
     @ride1.board_rider(@visitor2)
     @ride1.board_rider(@visitor1)
 
+    #Start straightforward:
     expect(@ride1.rider_log).to eq({@visitor1 => 2, @visitor2 => 1})
+    expect(@visitor1.spending_money).to eq(8)
+    expect(@visitor2.spending_money).to eq(4)
+    expect(@ride1.total_revenue).to eq(3)
+
+    #A little trickier:
+    @ride3.board_rider(@visitor1)
+    @ride3.board_rider(@visitor2)
+    @ride3.board_rider(@visitor3)
+
+    expect(@visitor1.spending_money).to eq(8)
+    expect(@visitor2.spending_money).to eq(4)
+    expect(@visitor3.spending_money).to eq(13)
+    expect(@ride3.rider_log).to eq({@visitor3 => 1})
+    expect(@ride3.total_revenue).to eq(2)
+
+    #Bruce gets brave...
+    @visitor1.add_preference(:thrilling)
+    @ride3.board_rider(@visitor1)
+    @ride3.board_rider(@visitor1)
+    expect(@ride3.rider_log).to eq({@visitor1 => 2, @visitor3 => 1})
+
+
+
   end
 
 end
